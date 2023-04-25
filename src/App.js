356f07da5a1b2c8f5e0e158ec52309b5870e481c
Fiona <57137659+fquinn454@users.noreply.github.com>
2023-04-25 19:07:34 +0100

    useReducer to make todo list

--- src/App.js
+++ src/App.js
@@ -1,42 +1,60 @@
-import React, { useReducer } from 'react';
+import React, { useState, useReducer } from 'react';
+import Todo from './components/Todo'
 import './App.css';
 
-const ACTIONS = {
-  INCREMENT: 'increment',
-  DECREMENT: 'decrement'
+export const ACTIONS = {
+  ADD_TODO: 'add-todo',
+  TOGGLE_TODO: 'toggle-todo',
+  DELETE_TODO: 'delete-todo'
 }
 
-function reducer(state, action){
-  switch (action.type){
-    case ACTIONS.INCREMENT:
-      return { count: state.count + 1 }
+function reducer(todos, action){
+  switch(action.type){
+    case ACTIONS.ADD_TODO:
+      return [...todos, newTodo(action.payload.name)]
 
-    case ACTIONS.DECREMENT:
-      return { count: state.count - 1 }
+    case(ACTIONS.TOGGLE_TODO):
+      return todos.map(todo => {
+        if (todo.id === action.payload.id) {
+          return { ...todo, complete: !todo.complete}
+        }
+        return todo
+      })
     
-    default:
-      return state
+      case(ACTIONS.DELETE_TODO):
+        return todos.filter(todo => todo.id !== action.payload.id );
+
+      default:
+        return todos
+
   }
-  
 }
+
+function newTodo(name){
+  return {id: Date.now(), name: name, complete:false}
+}
+
 
 function App() {
-  const[state, dispatch] = useReducer(reducer, { count:0 });
+  const[todos, dispatch] = useReducer(reducer, []);
+  const[name, setName] = useState('')
   
-
-  function increment(){
-    dispatch({type: ACTIONS.INCREMENT})
+  function handleSubmit(e){
+    e.preventDefault();
+    dispatch({type: ACTIONS.ADD_TODO, payload: { name:name } });
+    setName('');
   }
 
-  function decrement(){
-    dispatch({type: ACTIONS.DECREMENT})
-  }
+  console.log(todos);
 
   return (
     <>
-      <button onClick={decrement}>-</button>
-      <span>{state.count}</span>
-      <button onClick={increment}>+</button>
+      <form onSubmit={handleSubmit}>
+        <input type='text' value={name} onChange={e => setName(e.target.value)} />
+      </form>
+      {todos.map(todo => {
+        return <Todo key={todo.id} todo={todo} dispatch={dispatch} />
+      })}
     </>
   )
 }
